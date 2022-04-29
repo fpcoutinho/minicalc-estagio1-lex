1916de60c49a7143661c2aa134b9b9402964784d
Filipe Coutinho <fpcoutinho5@gmail.com>
2022-04-29 17:37:33 -0300

    Initial commit

---
+++ lexer.h
@@ -0,0 +1,34 @@
+//
+// Analisador lexico
+//
+
+#ifndef MINICALC_LEXER_H
+#define MINICALC_LEXER_H
+
+typedef enum tagTipoToken {
+    TOKEN_INT,
+    TOKEN_PRINT,
+    TOKEN_ERRO,
+    TOKEN_SOMA,
+    TOKEN_MULT,
+    TOKEN_ABREPAR,
+    TOKEN_FECHAPAR,
+    TOKEN_EOF,
+    TOKEN_SUB,
+    TOKEN_DIV,
+    TOKEN_ABRECOL,
+    TOKEN_FECHACOL
+} TipoToken;
+
+typedef struct tagToken {
+    TipoToken tipo;
+    int       valor;
+} Token;
+
+
+void InicializaLexer(char *arqFonte);
+Token* ProximoToken();
+void FinalizaLexer();
+
+
+#endif //MINICALC_LEXER_H
